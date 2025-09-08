183c83379335a31bccfdf9bbfa1daed7030699fd
Felipe Gonzalez <cirk15@hotmail.com>
2025-09-08 18:53:05 -0500

    Se crea  html del formulario, ya anteriormente se envio el JS

--- TechTraking/src/main/java/com/devs/TechTraking/model/Revision.java
+++ TechTraking/src/main/java/com/devs/TechTraking/model/Revision.java
@@ -13,7 +13,6 @@ public class Revision {
     // ✅ Checklist (31 preguntas Sí/No del documento)
     private boolean equipoEnciende;
     private boolean displayFunciona;
-    private boolean alarmasActivas-;
     private boolean presentaFugas;-
     private boolean cablesEnBuenEstado;
     private boolean nivelesCorrectos;
