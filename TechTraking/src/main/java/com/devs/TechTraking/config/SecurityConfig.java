d18192bd2fc817d08d00d1bf74e4b7b1e9affb58
Jefferson Gomez Luna <jeffersongomezluna@gmail.com>
2025-09-27 23:01:47 -0500

    se realiza configuración en la base de datos para que se registre la hora de los inicios de sesión.

--- TechTraking/src/main/java/com/devs/TechTraking/config/SecurityConfig.java
+++ TechTraking/src/main/java/com/devs/TechTraking/config/SecurityConfig.java
@@ -6,8 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
@@ -72,11 +70,6 @@ public class SecurityConfig {
                )
                .build();
 
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 
     @Bean
